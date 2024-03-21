7fa4df4a6be44b4a9ac25691fc42b88c2dd58c99
Александр Жуховицкий <adzhukhovitskij@mai.education>
2024-03-21 13:33:12 +0300

    fix: add asyncio for async server

--- event_service/main.py
+++ event_service/main.py
@@ -1,14 +1,20 @@
 import logging
+import asyncio
 import grpc
-from concurrent import futures
 
 import proto.event_service_pb2_grpc as event_service_grpc
 from src.event_service_impl import EventServiceImpl
 
-if __name__ == "__main__":
-    logging.basicConfig()
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+async def serve():
+    """Start an async server"""
+    server = grpc.aio.server()
     event_service_grpc.add_EventServiceServicer_to_server(EventServiceImpl(), server)
     server.add_insecure_port("0.0.0.0:8081")
-    server.start()
-    server.wait_for_termination()
+    await server.start()
+    await server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    asyncio.run(serve())
