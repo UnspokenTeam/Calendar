e594cf5079dce35d3a4503d44e98e4d377057f11
Александр Жуховицкий <adzhukhovitskij@mai.education>
2024-03-23 00:52:35 +0300

    feat: create db handler for event service

--- event_service/main.py
+++ event_service/main.py
@@ -3,6 +3,7 @@ import asyncio
 import grpc
 
 import proto.event_service_pb2_grpc as event_service_grpc
+from db.db import Db
 from src.event_service_impl import EventServiceImpl
 
 
@@ -11,6 +12,7 @@ async def serve():
     server = grpc.aio.server()
     event_service_grpc.add_EventServiceServicer_to_server(EventServiceImpl(), server)
     server.add_insecure_port("0.0.0.0:8081")
+    await Db().connect()
     await server.start()
     await server.wait_for_termination()
 
