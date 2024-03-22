3f41a0376e4faadd8f43f9d364a578d0a5743954
Александр Жуховицкий <adzhukhovitskij@mai.education>
2024-03-23 00:57:49 +0300

    feat: add logging for the start of the application

--- event_service/main.py
+++ event_service/main.py
@@ -1,5 +1,7 @@
 import logging
 import asyncio
+import sys
+
 import grpc
 
 import proto.event_service_pb2_grpc as event_service_grpc
@@ -14,9 +16,13 @@ async def serve():
     server.add_insecure_port("0.0.0.0:8081")
     await Db().connect()
     await server.start()
+    logging.info("Server started on http://localhost:8081")
     await server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    logging.basicConfig()
+    logging.basicConfig(
+        level=logging.INFO,
+        handlers=[logging.StreamHandler(sys.stdout)]
+    )
     asyncio.run(serve())
