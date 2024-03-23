56830bc832b6f0c61fb1a98c033ec38648441592
Александр Жуховицкий <149195961+ZixMai@users.noreply.github.com>
2024-03-23 21:43:44 +0300

    feat: create db handler for event service (#3)
    * feat: create db handler for event service
    
    * feat: add logging for the start of the application
    
    * fix: PEP-8

--- event_service/main.py
+++ event_service/main.py
@@ -1,8 +1,11 @@
 import logging
 import asyncio
+import sys
+
 import grpc
 
 import proto.event_service_pb2_grpc as event_service_grpc
+from db.db import Db
 from src.event_service_impl import EventServiceImpl
 
 
@@ -11,10 +14,14 @@ async def serve():
     server = grpc.aio.server()
     event_service_grpc.add_EventServiceServicer_to_server(EventServiceImpl(), server)
     server.add_insecure_port("0.0.0.0:8081")
+    await Db().connect()
     await server.start()
+    logging.info("Server started on http://localhost:8081")
     await server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    logging.basicConfig()
+    logging.basicConfig(
+        level=logging.INFO, handlers=[logging.StreamHandler(sys.stdout)]
+    )
     asyncio.run(serve())
