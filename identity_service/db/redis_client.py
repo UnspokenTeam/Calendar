826ff1e9a241009f11eae0a1460cbf18f55e7f32
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2024-03-24 10:27:49 +0300

    feat: add docstrings to RedisClient

--- identity_service/db/redis_client.py
+++ identity_service/db/redis_client.py
@@ -4,10 +4,20 @@ from redis import Redis
 
 
 class RedisClient:
-    _db: Redis
+    """
+    Data class that stores user information
+
+    Attributes
+    ----------
+    db : redis.Redis
+        Redis client
+
+    """
+
+    db: Redis
 
     def __init__(self):
-        self._db = Redis(
+        self.db = Redis(
             host=os.environ["REDIS_HOST"],
             port=os.environ["REDIS_PORT"],
             socket_connect_timeout=2,
