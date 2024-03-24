b46b97574a0b1443dbd7787ae360cb9748d89f3d
Александр Жуховицкий <adzhukhovitskij@mai.education>
2024-03-24 18:36:48 +0300

    feat: create event repository and edit docstrings and edit import format and add some typing and change singleton class and change prisma schema

--- event_service/main.py
+++ event_service/main.py
@@ -5,16 +5,16 @@ import sys
 import grpc
 
 import proto.event_service_pb2_grpc as event_service_grpc
-from db.db import Db
+from db.postgres_client import PostgresClient
 from src.event_service_impl import EventServiceImpl
 
 
 async def serve():
-    """Start an async server"""
+    """Start an async server."""
     server = grpc.aio.server()
     event_service_grpc.add_EventServiceServicer_to_server(EventServiceImpl(), server)
     server.add_insecure_port("0.0.0.0:8081")
-    await Db().connect()
+    await PostgresClient().connect()
     await server.start()
     logging.info("Server started on http://localhost:8081")
     await server.wait_for_termination()
