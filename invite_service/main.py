1cca88a1692ff4789d02c1adfda95aab8ae36f68
Skelet0no <syefimov10@gmail.com>
2024-03-24 14:01:42 +0300

    fix: switch to async server

--- invite_service/main.py
+++ invite_service/main.py
@@ -1,14 +1,25 @@
 import logging
+import asyncio
+import sys
+
 import grpc
-from concurrent import futures
 
 import proto.invite_service_pb2_grpc as invite_service_grpc
 from src.invite_service_impl import InviteServiceImpl
 
-if __name__ == "__main__":
-    logging.basicConfig()
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+async def serve():
+    """Start an async server"""
+    server = grpc.aio.server()
     invite_service_grpc.add_InviteServiceServicer_to_server(InviteServiceImpl(), server)
     server.add_insecure_port("0.0.0.0:8082")
-    server.start()
-    server.wait_for_termination()
+    await server.start()
+    logging.info("Server started on http://localhost:8082")
+    await server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, handlers=[logging.StreamHandler(sys.stdout)]
+    )
+    asyncio.run(serve())
