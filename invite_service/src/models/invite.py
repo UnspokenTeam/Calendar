f45f89e857bdd719efa51ea7db343989e0112e24
Skelet0no <syefimov10@gmail.com>
2024-03-24 14:34:59 +0300

    fix: add docstring in invite.py

--- invite_service/src/models/invite.py
+++ invite_service/src/models/invite.py
@@ -1,9 +1,34 @@
+"""Invite Model"""
 from dataclasses import dataclass
 from proto.invite_service_pb2 import Invite as GrpcInvite, InviteStatus
 
 
 @dataclass
 class Invite:
+    """
+    Data class that stores user information
+
+    Attributes
+    ----------
+    id : str
+        ID of the user
+    event_id : str
+        ID of the event
+    author_id : str
+        ID of the author
+    invitee_id : str
+        ID of the invitee
+    status: InviteStatus:
+        Invite status
+
+
+    Methods
+    -------
+    to_grpc_invite()
+        Returns invite's information as a GrpcInvite class instance
+
+    """
+
     id: str
     event_id: str
     author_id: str
@@ -11,6 +36,14 @@ class Invite:
     status: InviteStatus
 
     def to_grpc_invite(self) -> GrpcInvite:
+        """
+        Converts invite information to GrpcInvite
+
+        Returns
+        -------
+        GrpcInvite
+            Invite data in GrpcInvite instance
+        """
         invite = GrpcInvite(
             id=self.id,
             event_id=self.event_id,
