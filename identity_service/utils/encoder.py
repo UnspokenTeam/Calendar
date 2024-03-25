fd7476813c11c8783693965a82efd90124efc5eb
Nhsdkk <nikittsev.anton@gmail.com>
2024-03-25 15:34:08 +0300

    feat: create encoder

---
+++ identity_service/utils/encoder.py
@@ -0,0 +1,56 @@
+"""Encoder class"""
+from bcrypt import gensalt, hashpw, checkpw
+
+
+class Encoder:
+    """
+    Encoder class
+
+    Methods
+    _______
+    encode(password)
+        Returns hashed password
+    compare(password, hashed_password)
+        Returns True if passwords match and False if not
+    """
+
+    @staticmethod
+    def encode(password: str) -> str:
+        """
+        Hash password
+
+        Parameters
+        ----------
+        password
+            Unhashed password
+
+        Returns
+        -------
+        str
+            Hashed password
+
+        """
+        salt = gensalt()
+        return hashpw(password=password.encode("UTF-8"), salt=salt).decode("UTF-8")
+
+    @staticmethod
+    def compare(password: str, hashed_password: str) -> bool:
+        """
+        Compares hashed and unhashed passwords
+
+        Parameters
+        ----------
+        password
+            Unhashed password
+        hashed_password
+            Hashed password
+
+        Returns
+        -------
+        bool
+            Flag if passwords are matching
+        """
+        return checkpw(
+            password=password.encode("UTF-8"),
+            hashed_password=hashed_password.encode("UTF-8"),
+        )
