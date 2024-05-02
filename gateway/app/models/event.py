7dbdf863aa83bd38d8ad0f03733ba29be3e98c95
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2024-05-02 23:34:40 +0300

    feat: modify methods and add update_event_as_admin and get_all_events

--- gateway/app/models/event.py
+++ gateway/app/models/event.py
@@ -1,9 +1,96 @@
 from datetime import datetime
-from typing import Optional, Self
+from typing import Optional, Self, Annotated
+from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, UUID4, AfterValidator, Field
+from pytz import utc
 
 from app.generated.event_service.event_service_pb2 import GrpcEvent
+from app.generated.event_service.event_service_pb2 import Interval as GrpcInterval
+
+
+class Interval(BaseModel):
+    """
+    Interval model
+
+    Attributes
+    ----------
+    years : int
+        Amount of years
+    months : int
+        Amount of months
+    weeks : int
+        Amount of weeks
+    days : int
+        Amount of days
+    hours : int
+        Amount of hours
+    minutes : int
+        Amount of minutes
+    seconds : int
+        Amount of seconds
+
+    Methods
+    -------
+    to_proto()
+        Converts the interval to proto.
+    from_proto()
+        Get Interval instance from proto.
+
+    """
+    years: Annotated[int, Field(0, ge=0)]
+    months: Annotated[int, Field(0, ge=0)]
+    weeks: Annotated[int, Field(0, ge=0)]
+    days: Annotated[int, Field(0, ge=0)]
+    hours: Annotated[int, Field(0, ge=0)]
+    minutes: Annotated[int, Field(0, ge=0)]
+    seconds: Annotated[int, Field(0, ge=0)]
+
+    def to_proto(self) -> GrpcInterval:
+        """
+        Converts the interval to proto.
+
+        Returns
+        -------
+        GrpcInterval
+            Proto interval
+
+        """
+        return GrpcInterval(
+            years=self.years,
+            months=self.months,
+            weeks=self.weeks,
+            days=self.days,
+            hours=self.hours,
+            minutes=self.minutes,
+            seconds=self.seconds,
+        )
+
+    @classmethod
+    def from_proto(cls, interval: GrpcInterval) -> Self:
+        """
+        Get Interval instance from proto
+
+        Parameters
+        ----------
+        interval : GrpcInterval
+            Proto interval
+
+        Returns
+        -------
+        Interval
+            Interval instance
+
+        """
+        return cls(
+            years=interval.years,
+            months=interval.months,
+            weeks=interval.weeks,
+            days=interval.days,
+            hours=interval.hours,
+            minutes=interval.minutes,
+            seconds=interval.seconds,
+        )
 
 
 class Event(BaseModel):
@@ -12,7 +99,7 @@ class Event(BaseModel):
 
     Attributes
     ----------
-    id : str
+    id : UUID4 | str
         Event ID
     title : str
         Event title
@@ -41,7 +128,7 @@ class Event(BaseModel):
         Get Event proto from Event instance
 
     """
-    id: str
+    id: UUID4 | Annotated[str, AfterValidator(lambda x: UUID(x, version=4))]
     title: str
     start: datetime
     end: datetime
@@ -49,7 +136,7 @@ class Event(BaseModel):
     created_at: datetime
     description: Optional[str] = None
     color: Optional[str] = None
-    repeating_delay: Optional[datetime] = None
+    repeating_delay: Optional[Interval] = None
     deleted_at: Optional[datetime] = None
 
     @classmethod
@@ -79,9 +166,7 @@ class Event(BaseModel):
             ),
             description=proto.description if proto.WhichOneof("optional_description") is not None else None,
             color=proto.color if proto.WhichOneof("optional_color") is not None else None,
-            repeating_delay=datetime.fromtimestamp(
-                proto.repeating_delay.seconds + proto.repeating_delay.nanos / 1e9
-            )
+            repeating_delay=Interval.from_proto(proto.repeating_delay)
             if proto.WhichOneof("optional_repeating_delay") is not None
             else None,
             deleted_at=datetime.fromtimestamp(
@@ -102,19 +187,18 @@ class Event(BaseModel):
 
         """
         event = GrpcEvent(
-            id=self.id,
+            id=str(self.id),
             title=self.title,
             author_id=self.author_id,
             description=self.description if self.description is not None else None,
             color=self.color if self.color is not None else None,
+            repeating_delay=self.repeating_delay.to_proto() if self.repeating_delay is not None else None,
         )
 
-        event.created_at.FromDatetime(self.created_at)
+        event.created_at.FromDatetime(self.created_at.astimezone(utc))
         event.start.FromDatetime(self.start)
         event.end.FromDatetime(self.end)
-        if self.repeating_delay is not None:
-            event.repeating_delay.FromDatetime(self.repeating_delay)
         if self.deleted_at is not None:
-            event.deleted_at.FromDatetime(self.deleted_at)
+            event.deleted_at.FromDatetime(self.deleted_at.astimezone(utc))
 
         return event
--- gateway/app/routers/events.py
+++ gateway/app/routers/events.py
@@ -1,22 +1,25 @@
 from datetime import datetime
 from typing import Annotated, List, Optional
+from uuid import UUID, uuid4
 
 from fastapi import APIRouter, Security, Depends
 from grpc import RpcError
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, UUID4, AfterValidator
+from pytz import utc
 
 from app.errors import PermissionDeniedError
 from app.generated.event_service.event_service_pb2 import (
     EventsRequestByAuthorId as GrpcGetEventsByAuthorIdRequest,
-    EventsResponse as GrpcGetEventsResponse,
+    EventsResponse as GrpcEventsResponse,
     EventsRequestByEventsIds as GrpcGetEventsRequestByEventsIdsRequest,
     ListOfEventsIds,
     EventResponse as GrpcEventResponse,
     EventRequestByEventId as GrpcGetEventByEventIdRequest,
-    DeleteEventRequest as GrpcDeleteEventRequest,
+    DeleteEventByIdRequest as GrpcDeleteEventByIdRequest,
     EventRequest as GrpcEventRequest,
     GenerateDescriptionRequest as GrpcGenerateDescriptionRequest,
     GenerateDescriptionResponse as GrpcGenerateDescriptionResponse,
+    GetAllEventsRequest as GrpcGetAllEventsRequest,
 )
 from app.generated.identity_service.get_user_pb2 import (
     UsersByIdRequest as GrpcGetUsersByIdsRequest,
@@ -27,17 +30,19 @@ from app.generated.invite_service.invite_service_pb2 import (
     InvitesResponse as GrpcGetInvitesResponse,
     DeleteInvitesByEventIdRequest as GrpcDeleteInvitesByEventIdRequest,
     InvitesByEventIdRequest as GrpcInvitesByEventIdRequest,
-    InviteStatus,
+    InviteStatus as GrpcInviteStatus,
 )
 from app.generated.notification_service.notification_service_pb2 import (
     DeleteNotificationsByEventIdRequest as GrpcDeleteNotificationsByEventIdRequest,
+    NotificationRequestByEventAndAuthorIds as GrpcGetNotificationByEventAndAuthorIdsRequest,
 )
 from app.generated.notification_service.notification_service_pb2 import (
     NotificationResponse as GrpcNotificationResponse,
 )
-from app.generated.user.user_pb2 import GrpcUser
+from app.generated.user.user_pb2 import GrpcUser, GrpcUserType
 from app.middleware import auth
 from app.models import User, Event
+from app.models.event import Interval
 from app.params import GrpcClientParams
 
 router = APIRouter(prefix="/events", tags=["events"])
@@ -87,19 +92,21 @@ class CreateEventRequest(BaseModel):
     end: datetime
     description: Optional[str]
     color: Optional[str]
-    repeating_delay: Optional[datetime]
+    repeating_delay: Optional[Interval]
 
 
 @router.get("/my/created")
 async def get_my_created_events(
+        page: Annotated[int, Field(1, ge=1)],
+        items_per_page: Annotated[int, Field(-1, ge=-1)],
         user: Annotated[GrpcUser, Security(auth)],
         grpc_clients: Annotated[GrpcClientParams, Depends(GrpcClientParams)],
-        page: int,
-        items_per_page: int,
         start: Optional[datetime] = None,
         end: Optional[datetime] = None,
 ) -> List[Event]:
     """
+    \f
+
     Fast api route to get all events created by a user.
 
     Parameters
@@ -123,29 +130,39 @@ async def get_my_created_events(
         List of events created by a user.
 
     """
-    my_events_result: GrpcGetEventsResponse = (
+    request_data = GrpcGetEventsByAuthorIdRequest(
+        author_id=user.id,
+        requesting_user=user,
+        page_number=page,
+        items_per_page=items_per_page,
+    )
+
+    if start is not None:
+        request_data.start.FromDatetime(start.astimezone(utc))
+    if end is not None:
+        request_data.end.FromDatetime(end.astimezone(utc))
+
+    my_events_result: GrpcEventsResponse = (
         grpc_clients.event_service_client.request().get_events_by_author_id(
-            GrpcGetEventsByAuthorIdRequest(
-                author_id=user.id,
-                requesting_user=user,
-                page_number=page,
-                items_per_page=items_per_page,
-            )
+            request_data
         )
     )
+
     return [Event.from_proto(event_proto) for event_proto in my_events_result.events.events]
 
 
 @router.get("/my/invited")
 async def get_my_invited_events(
+        page: Annotated[int, Field(1, ge=1)],
+        items_per_page: Annotated[int, Field(-1, ge=-1)],
         user: Annotated[GrpcUser, Security(auth)],
         grpc_clients: Annotated[GrpcClientParams, Depends(GrpcClientParams)],
-        page: int,
-        items_per_page: int,
         start: Optional[datetime] = None,
         end: Optional[datetime] = None,
 ) -> List[Event]:
     """
+    \f
+
     Fast api route to get all events where user is invited.
 
     Parameters
@@ -174,6 +191,9 @@ async def get_my_invited_events(
             GrpcGetInvitesByInviteeIdRequest(
                 invitee_id=user.id,
                 requesting_user=user,
+                page_number=1,
+                items_per_page=-1,
+                invite_status=GrpcInviteStatus.ACCEPTED,
             )
         )
     )
@@ -182,12 +202,13 @@ async def get_my_invited_events(
         item.event_id for item in invite_result.invites.invites
     )
 
-    invited_events_request: GrpcGetEventsResponse = (
+    invited_events_request: GrpcEventsResponse = (
         grpc_clients.event_service_client.request().get_events_by_events_ids(
             GrpcGetEventsRequestByEventsIdsRequest(
                 events_ids=ListOfEventsIds(ids=invited_events_id_list),
                 page_number=page,
-                items_per_page=items_per_page // 2 + items_per_page % 2,
+                items_per_page=items_per_page,
+
             )
         )
     )
@@ -197,11 +218,13 @@ async def get_my_invited_events(
 
 @router.get("/{id}")
 async def get_event(
-        event_id: str,
+        event_id: UUID4 | Annotated[str, AfterValidator(lambda x: UUID(x, version=4))],
         user: Annotated[GrpcUser, Security(auth)],
         grpc_clients: Annotated[GrpcClientParams, Depends(GrpcClientParams)],
 ) -> EventResponse:
     """
+    \f
+
     Fast api route to get a specific event.
 
     Parameters
@@ -222,7 +245,7 @@ async def get_event(
     event_response: GrpcEventResponse = (
         grpc_clients.event_service_client.request().get_event_by_event_id(
             GrpcGetEventByEventIdRequest(
-                event_id=event_id,
+                event_id=str(event_id),
                 requesting_user=user,
             )
         )
@@ -238,7 +261,8 @@ async def get_event(
         invited_people_request: GrpcGetInvitesResponse = (
             grpc_clients.invite_service_client.request().get_invites_by_event_id(
                 GrpcInvitesByEventIdRequest(
-                    event_id=event_id, invite_status=InviteStatus.ACCEPTED
+                    event_id=str(event_id),
+                    invite_status=GrpcInviteStatus.ACCEPTED
                 )
             )
         )
@@ -261,12 +285,75 @@ async def get_event(
     except RpcError:
         pass
 
-    # TODO: @Nhsdkk make request to get notification by event_id and author_id
-    notification_request: GrpcNotificationResponse = GrpcNotificationResponse()
+    notification_request: GrpcNotificationResponse = (
+        grpc_clients.notification_service_client.request().get_notification_by_event_and_author_ids(
+            GrpcGetNotificationByEventAndAuthorIdsRequest(
+                event_id=str(event_id),
+                author_id=user.id,
+                requesting_user=user
+            )
+        )
+    )
 
     response.notification_turned_on = notification_request.notification.enabled
 
     return response
+
+
+@router.get("/all/")
+async def get_all_events(
+        page: Annotated[int, Field(1, ge=1)],
+        items_per_page: Annotated[int, Field(-1, ge=-1)],
+        user: Annotated[GrpcUser, Security(auth)],
+        grpc_clients: Annotated[GrpcClientParams, Depends(GrpcClientParams)],
+        start: Optional[datetime] = None,
+        end: Optional[datetime] = None,
+) -> List[Event]:
+    """
+    \f
+
+    Get all events
+
+    Parameters
+    ----------
+    user : Annotated[GrpcUser, Security]
+        Authenticated user data.
+    grpc_clients : Annotated[GrpcClientParams, Depends(GrpcClientParams)]
+        Grpc clients injected by DI
+    page : int
+        Page number greater than 0.
+    items_per_page : int
+        Number of items per page. If -1 then all items are returned.
+    start : Optional[datetime]
+        Start date and time.
+    end : Optional[datetime]
+        End date and time.
+
+    Returns
+    -------
+    List[Event]
+        List of all events.
+
+    """
+    if user.type != GrpcUserType.ADMIN:
+        raise PermissionDeniedError("Permission denied")
+
+    events_request = GrpcGetAllEventsRequest(
+        requesting_user=user,
+        page_number=page,
+        items_per_page=items_per_page,
+    )
+
+    if start is not None:
+        events_request.start.FromDatetime(start.astimezone(utc))
+    if end is not None:
+        events_request.end.FromDatetime(end.astimezone(utc))
+
+    events_response: GrpcEventsResponse = grpc_clients.event_service_client.request().get_all_events(
+        events_request
+    )
+
+    return [Event.from_proto(event) for event in events_response.events.events]
 
 
 @router.get("/description/")
@@ -276,6 +363,8 @@ async def generate_event_description(
         grpc_clients: Annotated[GrpcClientParams, Depends(GrpcClientParams)],
 ) -> str:
     """
+    \f
+
     Fast api route to generate event description by AI.
 
     Parameters
@@ -302,12 +391,14 @@ async def generate_event_description(
 
 
 @router.post("/")
-def create_event(
+async def create_event(
         event_data: CreateEventRequest,
         user: Annotated[GrpcUser, Security(auth)],
         grpc_clients: Annotated[GrpcClientParams, Depends(GrpcClientParams)],
 ) -> None:
     """
+    \f
+
     Fast api route to create event.
 
     Parameters
@@ -321,7 +412,7 @@ def create_event(
 
     """
     event = Event(
-        id="",
+        id=uuid4(),
         title=event_data.title,
         start=event_data.start,
         end=event_data.end,
@@ -338,12 +429,14 @@ def create_event(
 
 
 @router.put("/")
-def update_event(
+async def update_event(
         event: Event,
         user: Annotated[GrpcUser, Security(auth)],
         grpc_clients: Annotated[GrpcClientParams, Depends(GrpcClientParams)],
 ) -> None:
     """
+    \f
+
     Fast api route to update event.
 
     Parameters
@@ -362,9 +455,47 @@ def update_event(
 
     """
     if event.author_id != user.id:
-        raise PermissionDeniedError
+        raise PermissionDeniedError("Permission denied")
+
+    db_event_response: GrpcEventResponse = grpc_clients.event_service_client.request().get_event_by_event_id(
+        GrpcGetEventByEventIdRequest(
+            event_id=str(event.id),
+            requesting_user=user,
+        )
+    )
+    db_event = Event.from_proto(db_event_response.event)
 
     event.deleted_at = None
+    event.created_at = db_event.created_at
+
+    grpc_clients.event_service_client.request().update_event(
+        GrpcEventRequest(event=event.to_proto(), requesting_user=user)
+    )
+
+
+@router.put("/admin/")
+async def update_event_as_admin(
+        event: Event,
+        user: Annotated[GrpcUser, Security(auth)],
+        grpc_clients: Annotated[GrpcClientParams, Depends(GrpcClientParams)],
+) -> None:
+    """
+    \f
+
+    Update event as admin
+
+    Parameters
+    ----------
+    event : Event
+        Event to update.
+    user : Annotated[GrpcUser, Security]
+        Authenticated user data.
+    grpc_clients : Annotated[GrpcClientParams, Depends(GrpcClientParams)]
+        Grpc clients injected by DI
+
+    """
+    if user.type != GrpcUserType.ADMIN:
+        raise PermissionDeniedError("Permission denied")
 
     grpc_clients.event_service_client.request().update_event(
         GrpcEventRequest(event=event.to_proto(), requesting_user=user)
@@ -372,17 +503,19 @@ def update_event(
 
 
 @router.delete("/")
-def delete_event(
-        event_id: str,
+async def delete_event(
+        event_id: UUID4 | Annotated[str, AfterValidator(lambda x: UUID(x, version=4))],
         user: Annotated[GrpcUser, Security(auth)],
         grpc_clients: Annotated[GrpcClientParams, Depends(GrpcClientParams)],
 ) -> None:
     """
+    \f
+
     Fast api route to delete event.
 
     Parameters
     ----------
-    event_id : str
+    event_id : UUID4 | str
         Event id.
     user : Annotated[GrpcUser, Security]
         Authenticated user data.
@@ -390,16 +523,16 @@ def delete_event(
         Grpc clients injected by DI
 
     """
-    grpc_clients.event_service_client.request().delete_event(
-        GrpcDeleteEventRequest(event_id=event_id, requesting_user=user)
-    )
-
     grpc_clients.notification_service_client.request().delete_notifications_by_event_id(
-        GrpcDeleteNotificationsByEventIdRequest(event_id=event_id, requesting_user=user)
+        GrpcDeleteNotificationsByEventIdRequest(event_id=str(event_id), requesting_user=user)
     )
 
     grpc_clients.invite_service_client.request().delete_invites_by_event_id(
         GrpcDeleteInvitesByEventIdRequest(
-            event_id=event_id,
+            event_id=str(event_id),
         )
+    )
+
+    grpc_clients.event_service_client.request().delete_event_by_id(
+        GrpcDeleteEventByIdRequest(event_id=str(event_id), requesting_user=user)
     )
