229f252168071371335563b2d5f9b54b8b4ca52f
Nhsdkk <nikittsev.anton@gmail.com>
2024-05-05 00:55:55 +0300

    feat: allow CORS

--- gateway/app/main.py
+++ gateway/app/main.py
@@ -1,10 +1,23 @@
 """Main file"""
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+
 from .middleware import InterceptorMiddleware
 from .params import GrpcClientParams
 from .routers import Events, Invites, Notifications, Users
 from fastapi import Depends, FastAPI
 
-app = FastAPI(dependencies=[Depends(GrpcClientParams)])
+app = FastAPI(
+    dependencies=[Depends(GrpcClientParams)],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.add_middleware(InterceptorMiddleware)
 
 app.include_router(Events)
