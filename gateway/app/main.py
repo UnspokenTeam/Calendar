3c1ea9650b91f831bc1325d6811592c50c44766b
Anton Nikittsev <38581029+Nhsdkk@users.noreply.github.com>
2024-05-05 18:48:51 +0300

    feat: allow CORS in gateway (#100)
    * feat: allow CORS
    
    * fix: linter and add python-multipart

--- gateway/app/main.py
+++ gateway/app/main.py
@@ -3,8 +3,19 @@ from .middleware import InterceptorMiddleware
 from .params import GrpcClientParams
 from .routers import Events, Invites, Notifications, Users
 from fastapi import Depends, FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI(dependencies=[Depends(GrpcClientParams)])
+app = FastAPI(
+    dependencies=[Depends(GrpcClientParams)],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.add_middleware(InterceptorMiddleware)
 
 app.include_router(Events)
