bb05bb1ed90286ae072e5531d0124a392ac40f95
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2024-05-10 14:33:36 +0300

    fix: linter

--- identity_service/src/models/user.py
+++ identity_service/src/models/user.py
@@ -10,8 +10,6 @@ from prisma.models import User as PrismaUser
 from src.generated.identity_service.update_user_pb2 import UserToModify as GrpcUserToModify
 from src.generated.user.user_pb2 import GrpcUser, GrpcUserType
 
-from pytz import utc
-
 
 class UserType(StrEnum):
     """
