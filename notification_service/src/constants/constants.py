f9082ad1a61e0ee5f64c34f2fb14fee66bf3e915
Александр Жуховицкий <149195961+ZixMai@users.noreply.github.com>
2024-05-11 16:58:28 +0300

    feat: add delay to event field for notification (#122)

--- notification_service/src/constants/constants.py
+++ notification_service/src/constants/constants.py
@@ -13,8 +13,8 @@ INTERVAL_SLOTS = (
 # fmt: off
 GET_NOTIFICATIONS_BY_AUTHOR_ID_QUERY = (
     "SELECT pattern.\"id\", pattern.\"event_id\", pattern.\"author_id\","
-    " pattern.\"notification_start\" as \"start\", pattern.\"repeating_delay\", pattern.\"enabled\", "
-    "pattern.\"created_at\", pattern.\"deleted_at\"\nFROM (\n\tSELECT *\n\tFROM "
+    " pattern.\"notification_start\" as \"start\", pattern.\"delay_to_event\", pattern.\"repeating_delay\", "
+    "pattern.\"enabled\", pattern.\"created_at\", pattern.\"deleted_at\"\nFROM (\n\tSELECT *\n\tFROM "
     "\"PrismaNotification\" as notification,\n\t\tGENERATE_SERIES(notification.start, {}, "
     "notification.repeating_delay::interval) as \"notification_start\"\n\tWHERE notification.repeating_delay IS"
     " NOT NULL\n) as pattern\nWHERE\n\tpattern.author_id = {}\n\tAND pattern.enabled = TRUE\n\tAND pattern.deleted_at "
