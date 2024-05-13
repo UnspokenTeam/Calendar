3ed94f60be88146b1d3ba9fdf85266ecaae72685
Александр Жуховицкий <adzhukhovitskij@mai.education>
2024-05-13 20:06:23 +0300

    feat: change db name

--- notification_service/src/constants/constants.py
+++ notification_service/src/constants/constants.py
@@ -15,7 +15,7 @@ GET_NOTIFICATIONS_BY_AUTHOR_ID_QUERY = (
     "SELECT pattern.\"id\", pattern.\"event_id\", pattern.\"author_id\","
     " pattern.\"notification_start\" as \"start\", pattern.\"delay_to_event\", pattern.\"repeating_delay\", "
     "pattern.\"enabled\", pattern.\"created_at\", pattern.\"deleted_at\"\nFROM (\n\tSELECT *\n\tFROM "
-    "\"PrismaNotification\" as notification,\n\t\tGENERATE_SERIES(notification.start, {}, "
+    "\"prisma_notifications\" as notification,\n\t\tGENERATE_SERIES(notification.start, {}, "
     "notification.repeating_delay::interval) as \"notification_start\"\n\tWHERE notification.repeating_delay IS"
     " NOT NULL\n) as pattern\nWHERE\n\tpattern.author_id = {}\n\tAND pattern.enabled = TRUE\n\tAND pattern.deleted_at "
     "IS NULL{}{}\nORDER BY start{};"
@@ -29,7 +29,7 @@ GET_EVENTS_BY_AUTHOR_ID_QUERY example:
     pattern."delay_to_event", pattern."repeating_delay", pattern."enabled", pattern."created_at", pattern."deleted_at"
     FROM (
         SELECT *
-        FROM "PrismaNotification" as notification,
+        FROM "prisma_notifications" as notification,
             GENERATE_SERIES(notification.start, '01/01/1970 05:00:00'::timestamp,
             notification.repeating_delay::interval) as "notification_start"
         WHERE notification.repeating_delay IS NOT NULL
